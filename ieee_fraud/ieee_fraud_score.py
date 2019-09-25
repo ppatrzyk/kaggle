312e21a05b703f3336ac18d075008e957ba159bd
Piotr Patrzyk <piotr@patrzyk.me>
2019-09-25 19:22:15 +0200

    models v4

--- ieee_fraud/ieee_fraud_score.py
+++ ieee_fraud/ieee_fraud_score.py
@@ -12,21 +12,12 @@ if __name__ == '__main__':
 
 	print(f'transformed and splitted: {round(time.time()-start, 2)} secs from start')
 
-	lgbm_200 = joblib.load('lgbm_200.joblib')
-	lgbm_200_probs = ["{:.5f}".format(prob) for prob in lgbm_200.predict_proba(X_test)[:,1]]
-	lgbm_200_submit = pd.DataFrame({
-		'TransactionID': trans_id,
-		'isFraud': lgbm_200_probs
-	})
-	lgbm_200_submit.to_csv('lgbm_200_submit.csv', index=False, header=True)
-	print(f'lgbm_200 processed: {round(time.time()-start, 2)} secs from start')
-
-	lgbm_500 = joblib.load('lgbm_500.joblib')
-	lgbm_500_probs = ["{:.5f}".format(prob) for prob in lgbm_500.predict_proba(X_test)[:,1]]
-	lgbm_500_submit = pd.DataFrame({
-		'TransactionID': trans_id,
-		'isFraud': lgbm_500_probs
-	})
-	lgbm_500_submit.to_csv('lgbm_500_submit.csv', index=False, header=True)
-	print(f'lgbm_500 processed: {round(time.time()-start, 2)} secs from start')
-	
+	for estimators in [100, 150, 200, 300, 400]:
+		lgbm = joblib.load(f'lgbm_{estimators}.joblib')
+		lgbm_probs = ["{:.5f}".format(prob) for prob in lgbm.predict_proba(X_test)[:,1]]
+		lgbm_submit = pd.DataFrame({
+			'TransactionID': trans_id,
+			'isFraud': lgbm_probs
+		})
+		lgbm_submit.to_csv(f'lgbm_{estimators}_submit.csv', index=False, header=True)
+		print(f'lgbm_{estimators} processed: {round(time.time()-start, 2)} secs from start')
