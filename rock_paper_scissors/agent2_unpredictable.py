f857deb53a667d058aa7a34d8b66ee7d8a7e9c06
Piotr Patrzyk <piotr@patrzyk.me>
2020-11-06 15:19:31 +0100

    code for tracking actions

--- rock_paper_scissors/agent2_unpredictable.py
+++ rock_paper_scissors/agent2_unpredictable.py
@@ -8,10 +8,20 @@ import random
 # paper 1
 # scissors 2
 
+enemy_actions = []
+my_actions = []
+
 def random_agent(observation, configuration):
     action = random.choice((0, 1, 2))
     return action
 
 def agent(observation, configuration):
+    global enemy_actions
+    global my_actions
+    if observation.step == 0:
+        return random_agent(observation, configuration)
+    enemy_actions.append(observation.lastOpponentAction)
     current_agent = random_agent
-    return current_agent(observation, configuration)
+    action = current_agent(observation, configuration)
+    my_actions.append(action)
+    return action
